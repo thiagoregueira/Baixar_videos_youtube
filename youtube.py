328f80519b98938c932cc98d2aa061e9946cd3d0
thiagoregueira <thiago.regueira@yahoo.com.br>
2024-07-04 11:14:08 -0300

    refatorando

--- youtube.py
+++ youtube.py
@@ -1,37 +1,43 @@
-import io
+import os  # noqa: I001, F401
+from io import BytesIO
 
 import streamlit as st
 from pytube import YouTube
 
-st.title('YouTube Video Download')
 
-video_url = st.text_input('Insira o link do vídeo do YouTube:')
+def get_video_streams(url):
+    yt = YouTube(url)
+    streams = yt.streams.get_highest_resolution()
+    return streams
 
 
-def download_video(url):
+def download_video(stream):
+    buffer = BytesIO()
+    stream.stream_to_buffer(buffer)
+    buffer.seek(0)
+    return buffer
+
+
+st.title('YouTube Download Videos')
+
+# Input URL
+url = st.text_input('Digite o endereço do vídeo:')
+
+if url:
     try:
-        yt = YouTube(url)
-        stream = yt.streams.get_highest_resolution()
+        streams = get_video_streams(url)
 
-        # Baixa o vídeo para um buffer em memória
-        buffer = io.BytesIO()
-        stream.stream_to_buffer(buffer)
-        buffer.seek(0)
-
-        return True, buffer, stream.title
+        if st.button('Download Buffer'):
+            with st.spinner('Downloading...'):
+                video_buffer = download_video(streams)
+                st.download_button(
+                    label='Download Video',
+                    data=video_buffer,
+                    file_name=f'{streams.title}.mp4',
+                    mime='video/mp4',
+                )
+                st.success(
+                    'Download pronto! Clique no botão acima para baixar o vídeo.'  # noqa: E501
+                )
     except Exception as e:
-        return False, None, f'Erro no download do video: {str(e)}'
-
-
-if st.button('Download Video'):
-    success, buffer, message = download_video(video_url)
-    if success:
-        st.download_button(
-            label='Baixar Vídeo',
-            data=buffer,
-            file_name=f'{message}.mp4',
-            mime='video/mp4',
-        )
-        st.success(f'Download do video: {message} feito com sucesso!')
-    else:
-        st.error(message)
+        st.error(f'Error: {e}')
